01fa83327657fbb74a78ce956de7f3a7af940114
Ignacio911 <Ignacio911@gmail.com>
2018-05-30 07:10:11 +0000

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# https://github.com/Ignacio911/assetsrc70.pythonanywhere.com/blob/master/sitemap.xml.gz?raw=true
+# https://assetsrc70.pythonanywhere.com/sitemap.xml.gz
---
+++ pinglist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrc70.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrc70.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrc70.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrc70.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrc70.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrc70.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrc70.netlify.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrc70.netlify.com/sitemap.xml.gz
